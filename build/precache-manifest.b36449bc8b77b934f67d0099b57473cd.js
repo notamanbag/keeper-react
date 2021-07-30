db63ae06c3b61c5b3176fd55653e7aa23218ad1b
Aman Bagrecha <amanbagrecha@outlook.com>
2021-07-30 15:11:07 +0530

    Deeployed

---
+++ build/precache-manifest.b36449bc8b77b934f67d0099b57473cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43549d28fe96e40e17ac16608276e2e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49118ddb4158c7681da1",
+    "url": "/static/js/2.ba31994b.chunk.js"
+  },
+  {
+    "revision": "99b81bab5752255746a2",
+    "url": "/static/js/main.5f0493e9.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
